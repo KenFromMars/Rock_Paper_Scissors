1522798257393106f5e6c4d7f6b8ec218f2b0a5c
KenFromMars <61832421+KenFromMars@users.noreply.github.com>
2024-11-05 20:21:35 +0000

    Game Logic
    function to make the game 5 round, modifying if else statement

--- js/javascript.js
+++ js/javascript.js
@@ -17,96 +17,58 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let humanChoice = prompt("enter your choice (rock,paper,scissors").toLowerCase();
-    
 
-    // if(humanChoice===0){
-    //     return "rock";
-    // }
-    // else if(humanChoice===2){
-    //     return "scissors";
-    // }
-    // else if (humanChoice===5){
-    //     return "paper";
-    // }
-    // else{
-    //     return getHumanChoice();
-    // }
     return humanChoice;
 }
 // intialize score to 0
 let humanScore = 0;
 let computerScore =0;
 function playRound(humanInput,computerInput){
-    if(humanInput ==="rock"){
-        if(computerInput ==="rock"){
-            return {
-                humanScore: humanScore,
-                computerScore: computerScore
-            };
-        }
-        else if(computerInput ==="paper"){
-            return {
-                humanScore: humanScore,
-                computerScore: ++computerScore
-            };
-        }
-        else{
-            return {
-                humanScore: ++humanScore,
-                computerScore: computerScore
-            };
-        }
+    if(humanInput===computerInput){
+        console.log(`TRY AGAIN! ${humanInput} CAN'T BEAT ${computerInput}.`)
+        return {
+            humanScore: humanScore,
+            computerScore: computerScore
+        };
     }
-
-    if(humanInput ==="scissors"){
-        if(computerInput ==="rock"){
-            return {
-                humanScore: humanScore,
-                computerScore: ++computerScore
-            };
-        }
-        else if(computerInput ==="paper"){
-            return {
-                humanScore: ++humanScore,
-                computerScore: computerScore
-            };
-        }
-        else{
-            return {
-                humanScore: humanScore,
-                computerScore: computerScore
-            };
-        }
+    else if (humanInput==="rock" && computerInput ==="scissors" ||
+             humanInput==="scissors" && computerInput ==="paper" ||
+             humanInput ==="paper" && computerInput ==="rock" ){
+                console.log(`YEAH YOU GOT IT! ${humanInput} BEATS ${computerInput}.`)
+                return {
+                    humanScore: ++humanScore,
+                    computerScore: computerScore
+                };
+    }
+    else {
+        console.log(`YOU MISSED! ${computerInput} BEATS ${humanInput}.`)
+        return {
+            
+            humanScore: humanScore,
+            computerScore: ++computerScore
+        };
     }
     
-    if(humanInput ==="paper"){
-        if(computerInput ==="rock"){
-            return {
-                humanScore: ++humanScore,
-                computerScore: computerScore
-            };
-        }
-         
-        else if(computerInput ==="paper"){
-            return {
-                humanScore : humanScore,
-                computerScore: computerScore
-
-            };
-        }
-        else{
-            return {
-                humanScore: humanScore,
-                computerScore: ++computerScore
-            };
-        }
-    }
-    
-    console.log(humanScore)
-    console.log(computerScore)
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame(){
+    for(i=1;i<=5;i++){
+        playRound(getHumanChoice(),getComputerChoice() );
+    }
+    if(humanScore > computerScore){
+        console.log(`YOU WIN! ${humanScore}:${computerScore}`)
+    }
+    else if(computerScore>humanScore) {
+        console.log(`COMPUTER WINS!"${computerScore}:${humanScore}`)
+    }
+    else {
+        console.log(`TIE!"${computerScore}:${humanScore}`)
+    }
+    return {
+        humanScore: humanScore,
+        computerScore: computerScore
+    };
 
-console.log(playRound(humanSelection,computerSelection ))
+}
 
+
+console.log(playGame())
